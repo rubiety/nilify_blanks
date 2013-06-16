2fe6cb713968bf9fd03ed8d09f894311840a160d
Hamed Asghari <hasghari@g2labs.net>
2013-06-16 11:17:38 -0400

    removing redundant nil check

--- lib/nilify_blanks.rb
+++ lib/nilify_blanks.rb
@@ -2,26 +2,26 @@ module NilifyBlanks
   def self.included(base)
     base.extend ClassMethods
   end
-  
+
   module ClassMethods
     def nilify_blanks(options = {})
       return if self.included_modules.include?(NilifyBlanks::InstanceMethods)
       return unless self.table_exists?
-      
+
       include NilifyBlanks::InstanceMethods
-      
+
       if options[:only]
         options[:only] = [options[:only]] unless options[:only].is_a?(Array)
         options[:only] = options[:only].map(&:to_s)
       end
-      
+
       if options[:except]
         options[:except] = [options[:except]] unless options[:except].is_a?(Array)
         options[:except] = options[:except].map(&:to_s)
       end
-      
+
       cattr_accessor :nilify_blanks_columns
-      
+
       if options[:only]
         self.nilify_blanks_columns = options[:only].clone
       else
@@ -29,19 +29,19 @@ module NilifyBlanks
       end
       self.nilify_blanks_columns -= options[:except] if options[:except]
       self.nilify_blanks_columns = self.nilify_blanks_columns.map(&:to_s)
-      
+
       options[:before] ||= :save
       class_eval "before_#{options[:before]} :nilify_blanks"
     end
   end
-  
+
   module InstanceMethods
     def nilify_blanks
       (self.nilify_blanks_columns || []).each do |column|
         value = read_attribute(column)
         next unless value.is_a?(String)
-        next unless value.nil? or !value.respond_to?(:blank)
-        
+        next unless value.respond_to?(:blank?)
+
         write_attribute(column, nil) if value.blank?
       end
     end
