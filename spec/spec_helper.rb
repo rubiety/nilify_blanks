e4aa5c2b51f3309758ab2149be59a3515adae672
Edward Tippett <edward.tippett@gmail.com>
2019-02-02 12:46:10 +1030

    Update RSpec to version 3
    Move to latest version of `RSpec` and update specs to the new `expect`
    syntax
    
    - Change specs to use `expect` with transpec
    - Update RSpec to version `3.8` and bump lockfiles
    - Update `spec_helper` with modern opt ins

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,28 @@ ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations['test'
 load(File.dirname(__FILE__) + "/db/schema.rb")
 
 require File.dirname(__FILE__) + '/../init'
+
+RSpec.configure do |config|
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures)
+  end
+
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  # config.warnings = true
+end
