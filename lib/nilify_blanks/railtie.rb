47fd8655851d9512b0e27bb0fa90e0cfc69025c2
Ben Hughes <me@benhughes.name>
2020-04-18 17:52:46 -0700

    Drop excess spaces in railtie

--- lib/nilify_blanks/railtie.rb
+++ lib/nilify_blanks/railtie.rb
@@ -1,7 +1,7 @@
 module NilifyBlanks
   if defined?(Rails::Railtie)
     require "rails"
-    
+
     class Railtie < Rails::Railtie
       initializer "nilify_blanks.extend_active_record" do
         ActiveSupport.on_load(:active_record) do
@@ -10,7 +10,7 @@ module NilifyBlanks
       end
     end
   end
-  
+
   class Railtie
     def self.insert
       ActiveRecord::Base.send(:include, NilifyBlanks)
